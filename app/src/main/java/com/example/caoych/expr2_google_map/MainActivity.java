3358ca03677c7335e6fe2a20403426a4bda363fa
Jostar1024 <howard.eureka@gmail.com>
2016-11-13 16:55:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/caoych/expr2_google_map/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.caoych.expr2_google_map;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void jumpToMap(View view) {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+
+        startActivity(intent);
+    }
+}
